905dd927c069c631fac09ced432f78a55d32a03d
NewtonJr4108 <71670138+NewtonJr4108@users.noreply.github.com>
2021-05-27 20:20:33 -0400

    Update transaction.cmd

--- transaction.cmd
+++ transaction.cmd
@@ -1,4 +1,4 @@
-curl --location --request POST 'localhost:5000/transaction' \
+curl --location --request POST "localhost:5000/transaction" \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "recipient":"recipient-pub-address",
