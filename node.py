1cadc2904d87d768faf41179c4819fc3af131899
NewtonJr4108 <71670138+NewtonJr4108@users.noreply.github.com>
2021-06-04 17:18:48 -0400

    Update node.py

--- node.py
+++ node.py
@@ -268,4 +268,4 @@ if __name__ == '__main__':
     port = args.port
     wallet = Wallet(port)
     blockchain = Blockchain(wallet.public_key, port)
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port)
