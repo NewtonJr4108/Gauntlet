3664673f80bdf3e336be6746045860e178b0e04f
NewtonJr4108 <71670138+NewtonJr4108@users.noreply.github.com>
2021-06-19 17:40:32 -0400

    Update wallet.py
    Avoided Import Error while increasing security of SHA1 wallet encryption.

--- wallet.py
+++ wallet.py
@@ -1,6 +1,6 @@
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
-from Crypto.Hash import SHA512
+from Crypto.Hash import SHA1
 import Crypto.Random
 import binascii
 
@@ -47,7 +47,7 @@ class Wallet:
 
     # Generate a new pair of private and public key
     def generate_keys(self):
-        private_key = RSA.generate(1024, Crypto.Random.new().read)
+        private_key = RSA.generate(4096, Crypto.Random.new().read)
         public_key = private_key.publickey()
         return (binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'), binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii'))
 
@@ -56,7 +56,7 @@ class Wallet:
     # binascii.hexlify is used to convert binary data to hexadecimal representation
     def sign_transaction(self, sender, recipient, amount):
         signer = PKCS1_v1_5.new(RSA.importKey(binascii.unhexlify(self.private_key)))
-        h = SHA512.new((str(sender) + str(recipient) + str(amount)).encode('utf8'))
+        h = SHA1.new((str(sender) + str(recipient) + str(amount)).encode('utf8'))
         signature = signer.sign(h)
         return binascii.hexlify(signature).decode('ascii')
 
@@ -65,5 +65,5 @@ class Wallet:
     def verify_transaction(transaction):
         public_key = RSA.importKey(binascii.unhexlify(transaction.sender))
         verifier = PKCS1_v1_5.new(public_key)
-        h = SHA512.new((str(transaction.sender) + str(transaction.recipient) + str(transaction.amount)).encode('utf8'))
+        h = SHA1.new((str(transaction.sender) + str(transaction.recipient) + str(transaction.amount)).encode('utf8'))
         return verifier.verify(h, binascii.unhexlify(transaction.signature))
