541d9c83a62b0822be187aac10174b455a515ca7
NewtonJr4108 <71670138+NewtonJr4108@users.noreply.github.com>
2021-06-23 10:51:36 -0400

    Delete transaction.cmd

--- transaction.cmd
+++
@@ -1,7 +0,0 @@
-curl --location --request POST "localhost:5000/transaction" \
---header 'Content-Type: application/json' \
---data-raw '{
-    "recipient":"recipient-pub-address",
-    "amount":5
-    "signature":"sender-pub-address"
-}'
